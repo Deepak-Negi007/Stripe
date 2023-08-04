73775352aedb9ad0fc3cacfde52f0d7108b82b45
Deepak <coldhearteddeepaknegi@gmail.com>
2023-08-04 13:03:08 +0530

    Final Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FinalAccordian from './Accordian/FinalAccordian';
+// import './App.css';
+
+function App() {
+return (
+<>
+<FinalAccordian/>
+</>
+)}
+
+export default App;
